bbad85915e5c37dab5c572b3413f569eaa2e64d3
CristopherGS <guerracristofer@gmail.com>
2021-10-24 23:58:29 -0600

    se arreglo el link http para la busquedas

--- 05-gif-search-app/src/helpers/getGifs.js
+++ 05-gif-search-app/src/helpers/getGifs.js
@@ -4,7 +4,7 @@
 
 export const getGif = async (category) => {
     const key = 'S0eSGU5nsF6odoLn3Sxed6PzBhb9ZCGr';
-    const url = `https://api.giphy.com/v1/gifs/trending?q=${encodeURI(category)}&limit=5&api_key=${key}`
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=20&api_key=${key}`
     const respuesta = await fetch(url);
     const { data } = await respuesta.json();
 
